2ed8cd062913ea5e6047ba581bebbf7e95eacda1
Samuel Escobar <samuelescob200485@gmail.com>
2025-05-15 20:41:40 -0600

    Log of version

--- src/main/java/org/quantum/solutions/quantumsolutions/HelloApplication.java
+++ src/main/java/org/quantum/solutions/quantumsolutions/HelloApplication.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        System.out.println("Init aplication V1.0.0");
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setScene(scene);
